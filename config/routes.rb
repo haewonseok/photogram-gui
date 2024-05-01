9a8535b5b639a3a531635acb02a03acdd418a91e
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-05-01 04:31:19 +0000

    Completed except for 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   post("/insert_user", { :controller => "users", :action => "create" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
-  post("/modify_user/:path_id", { :controller => "users", :action => "update" })
+  post("/update_user/:path_id", { :controller => "users", :action => "update" })
 
   get("/photos", { :controller => "photos", :action => "index" })
   post("/insert_photo", { :controller => "photos", :action => "create" }) 
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
-  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "destroy" })
 end
