4dfc1109b322151c44ae2818fb70c032ee5e8947
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-05-01 03:06:18 +0000

    Updated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index" })
 
-  post("/users", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
   post("/insert_users", { :controller => "users", :action => "create" })
-  post("/users/:path_id", { :controller => "users", :action => "show" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
   post("/modify_users/:path_id", { :controller => "users", :action => "update" })
 
-  post("/photos", { :controller => "photos", :action => "index" })
+  get("/photos", { :controller => "photos", :action => "index" })
   post("/insert_photos", { :controller => "photos", :action => "create" }) 
-  post("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
   post("/modify_photos/:path_id", { :controller => "photos", :action => "update" })
   post("/delete_photos/:path_id", { :controller => "photos", :action => "destroy" })
 end
